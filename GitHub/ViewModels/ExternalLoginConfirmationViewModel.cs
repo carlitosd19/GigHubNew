1d11fa1446bed35212c4fe86bbcafd5e719fd168
Mac Beath <mac.beath@hotmail.com>
2019-05-07 18:58:41 -0400

    dsfdfgd

---
+++ GitHub/ViewModels/ExternalLoginConfirmationViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace GitHub.ViewModels
+{
+    using System.ComponentModel.DataAnnotations;
+
+    public class ExternalLoginConfirmationViewModel
+    {
+        [Required]
+        [Display(Name = "Email")]
+        public string Email { get; set; }
+    }
+}
