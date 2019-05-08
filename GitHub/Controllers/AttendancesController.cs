3af2cb7d84d375c359bf3afc936a58555ec418df
Mac Beath <mac.beath@hotmail.com>
2019-05-08 08:30:28 -0400

    a

---
+++ GitHub/Controllers/AttendancesController.cs
@@ -0,0 +1,38 @@
+﻿namespace GitHub.Controllers
+{
+    using GitHub.Models;
+    using Microsoft.AspNet.Identity;
+    using System.Linq;
+    using System.Web.Http;
+
+    [Authorize]
+    public class AttendancesController : ApiController
+    {
+        private readonly ApplicationDbContext _context;
+
+        public AttendancesController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        [HttpPost]
+        public IHttpActionResult Attend([FromBody]int gigId)
+        {
+            var userId = User.Identity.GetUserId();
+
+            if (_context.Attendances.Any(a => a.AttendeeId == userId && a.GigId == gigId)
+                return BadRequest("The Attendance already exists");
+
+            
+            var attendance = new Attendance
+            {
+                GigId = gigId,
+                AttendeeId = User.Identity.GetUserId()
+            };
+            _context.Attendances.Add(attendance);
+            _context.SaveChanges();
+
+            return Ok();
+        }
+    }
+}
